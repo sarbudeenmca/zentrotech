f9d735b498ca0b17f13d9fa9844ef05572433ec6
Sarbudeen <sarbudeenmca@gmail.com>
2025-02-05 18:22:31 +0400

    working on footer section

--- php/index.php
+++ php/index.php
@@ -5,16 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>zentrotech</title>
-
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Baskervville:ital@0;1&family=Poppins:wght@300;400;500;600;700&display=swap');
-    </style>
-
     <!-- Font Awesome CDN -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@200..900&family=Wix+Madefor+Display:wght@400..800&display=swap" rel="stylesheet">
+
     <link href="assets/css/style.css" rel="stylesheet">
 
     <link rel="icon" type="image/png" href="assets/img/favicon.png">
@@ -23,43 +23,59 @@
 <body class="bg-light">
 
     <header class="col-12 container">
-        <img src="assets/img/projotech-logo.png" class="logo">
+        <img src="assets/img/zentrotech.png" class="logo">
     </header>
-
-    <div class="col-12 container">
-        <div class="row">
-            <div class="col-12">
-                <h1>Your gateway to <span>quality education</span>
-                    <br>for <span>every stage</span> of learning.
-                </h1>
-            </div>
-        </div>
-    </div>
 
     <main class="col-12 container">
         <div class="row">
-            <div class="box-container">
-                <div class="box row">
-                    <div class="box-inner bg col-12 col-lg-6">
+            <div class="col-12">
+                <h2>We're currently performing some updates and improvements to</h2>
+                <h1>bring you a <span>better experience</span></h1>
+                <p>We'll be back online shortly. In the meantime, please feel free to reach out to us for any inquiries or services.</p>
+            </div>
+        </div>
 
+        <div class="row grid">
+            <div class="col-12 boxes">
+                <div class="row">
+                    <div class="col">
+                        <h3>Web<br>Design</h3>
                     </div>
-                    <div class="box-content  col-12 col-lg-6">
-                        <h2>Building strong foundations for young learners.</h2>
-                        <a href="#"><span>Explore the</span>School Section</a>
+                    <div class="col">
+                        <h3>Web<br>Development</h3>
+                    </div>
+                    <div class="col">
+                        <h3>Web<br>Applications</h3>
+                    </div>
+                    <div class="col">
+                        <h3>Digital<br>Marketing</h3>
                     </div>
                 </div>
-
-                <div class="box row">
-                    <div class="box-inner bg1 col-12 col-lg-6">
-
+                <div class="row">
+                    <div class="col">
+                        <h3>Branding</h3>
                     </div>
-                    <div class="box-content col-12 col-lg-6">
-                        <h2>Empowering the next generation of professionals.</h2>
-                        <a href="#"><span>Explore the</span>University Section</a>
+                    <div class="col">
+                        <h3>Graphic<br>Design</h3>
+                    </div>
+                    <div class="col">
+                        <h3>Advertising</h3>
+                    </div>
+                    <div class="col">
+                        <h3>Event<br>Solutions</h3>
                     </div>
                 </div>
             </div>
         </div>
+
+        <div class="row">
+            <div class="contact">
+                <p>For urgent inquiries or assistance, please contact us</p>
+                <h3><a href="mailto:admin@zentrotech.com">admin@zentrotech.com</a> <span></span> <a href="mailto:hello@zentrotech.com">hello@zentrotech.com</a></h3>
+                <h3><a href="tel:"></a></h3>
+            </div>
+        </div>
+
     </main>
 
     <!-- Bootstrap JS -->
