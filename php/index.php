e231a2dc6e1efa4f3fc40c946a591aab8ce7fb03
Sarbudeen <sarbudeenmca@gmail.com>
2025-02-05 16:53:07 +0400

    Initial Commit

---
+++ php/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>zentrotech</title>
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Baskervville:ital@0;1&family=Poppins:wght@300;400;500;600;700&display=swap');
+    </style>
+
+    <!-- Font Awesome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    <link rel="icon" type="image/png" href="assets/img/favicon.png">
+</head>
+
+<body class="bg-light">
+
+    <header class="col-12 container">
+        <img src="assets/img/projotech-logo.png" class="logo">
+    </header>
+
+    <div class="col-12 container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Your gateway to <span>quality education</span>
+                    <br>for <span>every stage</span> of learning.
+                </h1>
+            </div>
+        </div>
+    </div>
+
+    <main class="col-12 container">
+        <div class="row">
+            <div class="box-container">
+                <div class="box row">
+                    <div class="box-inner bg col-12 col-lg-6">
+
+                    </div>
+                    <div class="box-content  col-12 col-lg-6">
+                        <h2>Building strong foundations for young learners.</h2>
+                        <a href="#"><span>Explore the</span>School Section</a>
+                    </div>
+                </div>
+
+                <div class="box row">
+                    <div class="box-inner bg1 col-12 col-lg-6">
+
+                    </div>
+                    <div class="box-content col-12 col-lg-6">
+                        <h2>Empowering the next generation of professionals.</h2>
+                        <a href="#"><span>Explore the</span>University Section</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
